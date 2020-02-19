e14346dbf5d6f666e8add23859b7902146933cf2
Rob Hamilton <RobHamilton3rd@gmail.com>
2020-02-19 17:33:33 -0500

    eating cookies and making change

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python
 
 import sys
+import functools
 
+#@functools.lru_cache(maxsize=128)
 def making_change(amount, denominations):
-  pass 
+  #if the amount is negative or 0, return 1
+  if amount <=0:
+    return 1
+  #need to create a reference for all possible ways to make change
+  #done at length of amount+1 since range indexes at 0 to start
+  possibilities = [0 for x in range(amount+1)]
+  #there is 1 way to make change of 0, which is once.
+  possibilities[0] = 1
+  #then, a loop is done across all possible denomination types
+  for i in range(0, len(denominations)):
+    #within each denomination, a loop is done between that denomination's value and the total amount of change
+      for j in range(denominations[i], amount+1):
+        #for each value that falls within the possibilities table
+        #it is incrimented by the possibilities at that cell, minus the value of that denominated currency.
+        #this looks recursively backwards to how much is solveable for a given amount of coins, but to an amount previously computed
+        #of the total amount - removing that denomination of coins.
+        possibilities[j] += possibilities[j-denominations[i]]
+  return possibilities[amount]
 
 
 if __name__ == "__main__":
